89b95e00352b12df301d813cbed2a105af21dcc9
AartheeBalaji <aartheegunasekar@gmail.com>
2021-11-10 13:15:57 +0530

    testsample

---
+++ src/org/test/sample.java
@@ -0,0 +1,23 @@
+package org.test;
+
+
+public class sample {
+	public void tc1() {
+		System.out.println("method1");
+	}
+	public void tc2() {
+		System.out.println("method2");
+	}
+	public void tc3() {
+		System.out.println("method3");
+	}
+
+	
+	 public static void main(String[] args) {
+		 sample s = new sample();
+		 s.tc1();
+		 s.tc2();
+		 s.tc3();
+		 
+	 }
+}
